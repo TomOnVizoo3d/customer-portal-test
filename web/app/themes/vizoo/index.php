b8338faf54c81e47ebd238d3bc844aebfc65f68d
Tom Schüler <tom.schueler@vizoo3d.com>
2026-07-01 16:03:41 +0200

    Switch to bedrocks root

--- web/app/themes/vizoo/index.php
+++ web/app/themes/vizoo/index.php
@@ -17,19 +17,19 @@ get_header(); ?>
 <div class="landingpage-wrapper">
     <div id="super-categories" class="super-category-wrapper">
         <a href="<?php echo esc_url(get_category_link(get_cat_ID('Knowledge Base'))); ?>" class="super-category">
-            <img class="category-icon" src="./wp-content/themes/vizoo/img/book-solid.svg" alt="">
+            <img class="category-icon" src="./app/themes/vizoo/img/book-solid.svg" alt="">
             <span class="category-name">
                 <h2>Knowledge Base</h2>
             </span>
         </a>
         <a href="<?php echo esc_url(get_category_link(get_cat_ID('Downloads'))); ?>" class="super-category">
-            <img class="category-icon" src="./wp-content/themes/vizoo/img/download-solid.svg" alt="">
+            <img class="category-icon" src="./app/themes/vizoo/img/download-solid.svg" alt="">
             <span class="category-name">
                 <h2>Downloads</h2>
             </span>
         </a>
         <a href="<?php echo esc_url(get_category_link(get_cat_ID('News'))); ?>" class="super-category">
-            <img class="category-icon" src="./wp-content/themes/vizoo/img/newspaper-solid.svg" alt="">
+            <img class="category-icon" src="./app/themes/vizoo/img/newspaper-solid.svg" alt="">
             <span class="category-name">
                 <h2>News</h2>
             </span>
@@ -39,7 +39,7 @@ get_header(); ?>
 if ($userrole == "LicenseManager") :
     ?>
             <a href="<?php echo get_permalink(get_page_by_title('Licenses')); ?>" class="super-category">
-                <img class="category-icon" src="./wp-content/themes/vizoo/img/key-solid.svg" alt="">
+                <img class="category-icon" src="./app/themes/vizoo/img/key-solid.svg" alt="">
                 <span class="category-name">
                     <h2>Licenses</h2>
                 </span>
